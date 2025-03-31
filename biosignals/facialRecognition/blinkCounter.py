2a388a077a44bfd142fd9507d11ecde8c0007333
Fabian <103424111+MatrixRave@users.noreply.github.com>
2025-03-31 10:07:23 +0200

    feat(facialRecognition): add live plotting for blinks

--- biosignals/facialRecognition/blinkCounter.py
+++ biosignals/facialRecognition/blinkCounter.py
@@ -1,20 +1,21 @@
 from imageFeed import camera 
 from cvzone.FaceMeshModule import FaceMeshDetector
+from cvzone.PlotModule import LivePlot
 import cv2 as cv
 import time 
 
 faceDetector = FaceMeshDetector(staticMode= False, maxFaces=1, minDetectionCon= 0.5, minTrackCon= 0.5)
 
 # Landmark Punkte des rechten Auges 
-leftEyeLandmarks = [33, 246, 161, 160, 159, 158, 157, 173, 133, 155, 154, 153, 145, 144, 163, 7]    
+leftEyeLandmarks = [33, 246, 161, 160, 159, 158, 157, 173, 133, 155, 154, 153, 145, 144, 163, 7, 23]    
 # Landmarks zur Normalisierung um Abstände auszugleichen 
 leftLengthHor: list[int] = [159, 23]
 leftLengthVert: list[int] = [130, 243]
 
 # Landmark Punkte des rechten Auges
 rightEyeLandmakrs = [463, 398, 384, 385, 386, 387, 388, 466, 263, 249, 390, 373, 374, 380, 381, 382, 362]
-rightLengthHor: list[int] = []
-rightLengthVert: list[int] = []
+rightLengthHor: list[int] = [386, 253]
+rightLengthVert: list[int] = [359, 463]
 
 # Landmark Punkte der linken Iris
 leftIrisLandmarks = [474, 475, 477, 476]  
@@ -30,6 +31,8 @@ mouthLandmakrs = [0, 267, 269, 270, 409, 306, 375, 321, 405, 314, 17, 84, 181, 9
 
 prev_frame_time = 0
 new_frame_time = 0
+
+plot = LivePlot(1200,480, [10,40])
 
 while True : 
 
@@ -47,15 +50,22 @@ while True :
 			for id in landmark:
 				cv.circle(frame, face[id], 5, (255,0,255), cv.BORDER_DEFAULT)
 
-		lengthHor, _ = faceDetector.findDistance(face[leftLengthHor[0]], face[leftLengthHor[1]])
-		lengthVert, _ = faceDetector.findDistance(face[leftLengthVert[0]], face[leftLengthVert[1]])
-		cv.line(frame, face[leftLengthHor[0]], face[leftLengthHor[1]], (255,0,0), 3)
+		leftHor, _ = faceDetector.findDistance(face[leftLengthHor[0]], face[leftLengthHor[1]])
+		leftVert, _ = faceDetector.findDistance(face[leftLengthVert[0]], face[leftLengthVert[1]])
+		cv.line(frame, face[rightLengthHor[0]], face[rightLengthHor[1]], (255,0,0), 3)
 		cv.line(frame, face[leftLengthVert[0]], face[leftLengthVert[1]], (255,0,0), 3)
+		rightHor, _ = faceDetector.findDistance(face[rightLengthHor[0]], face[rightLengthHor[1]])
+		rightVert, _ = faceDetector.findDistance(face[rightLengthVert[0]], face[rightLengthVert[1]])
 
 		fps = 1/(new_frame_time-prev_frame_time)
 		prev_frame_time = new_frame_time
-
 		fps = str(int(fps))
+
+
+		leftBlinkRatio = (leftVert/leftHor)*10
+		rightBlinkRatio = (rightVert/rightHor)*10
+		blinkPlot = plot.update((leftBlinkRatio+rightBlinkRatio)/2)
+		cv.imshow('BlinkPlot', blinkPlot)
 
 		cv.putText(frame, fps, (7, 70), cv.FONT_HERSHEY_SIMPLEX, 3, (100,100,0), 3, cv.LINE_AA)
 

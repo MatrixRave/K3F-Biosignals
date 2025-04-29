973d51cdae221f2a382216acd43c37a3d075a0cc
Fabian <103424111+MatrixRave@users.noreply.github.com>
2025-04-29 11:24:45 +0200

    refactor(facialReco): store landmarks in extra file

--- biosignals/facialRecognition/mediapipeBlink.py
+++ biosignals/facialRecognition/mediapipeBlink.py
@@ -3,8 +3,8 @@ import cv2 as cv
 import mediapipe as mp
 import time
 import math
-from cvzone.PlotModule import LivePlot
 import plotting as plt
+import recognitionVariables as recoVars
 
 # MediaPipe Initialisierung
 mp_face_mesh = mp.solutions.face_mesh
@@ -13,19 +13,6 @@ face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, refi
 # Zeichnen optional
 mp_drawing = mp.solutions.drawing_utils
 
-# Gesichtslandmark-IDs
-rightEyeLandmarks = [33, 246, 161, 160, 159, 158, 157, 173, 133, 155, 154, 153, 145, 144, 163, 7, 23]
-rightLengthHor = [27, 23]
-rightLengthVert = [130, 243]
-
-leftyeLandmarks = [466, 398, 384, 385, 386, 387, 388, 466, 263, 249, 390, 373, 374, 380, 381, 382, 362]
-leftLengthHor = [257, 253]
-leftLengthVert = [359, 463]
-
-plot = LivePlot(1200, 480, [10, 40])
-
-prev_frame_time = 0
-new_frame_time = 0
 
 def get_landmark_coords(landmarks, idx, image_w, image_h):
     landmark = landmarks[idx]
@@ -39,7 +26,7 @@ while True:
     if not ret:
         break
 
-    new_frame_time = time.time()
+    recoVars.new_frame_time = time.time()
     frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     results = face_mesh.process(frame_rgb)
 
@@ -48,30 +35,30 @@ while True:
     if results.multi_face_landmarks:
         landmarks = results.multi_face_landmarks[0].landmark
 
-        for landmark_set in [leftEyeLandmarks, rightEyeLandmarks]:
+        for landmark_set in [recoVars.leftEyeLandmarks, recoVars.rightEyeLandmarks]:
             for id in landmark_set:
                 x, y = get_landmark_coords(landmarks, id, image_w, image_h)
                 cv.circle(frame, (x, y), 2, (255, 0, 255), cv.FILLED)
 
         # Linkes Auge
-        p1 = get_landmark_coords(landmarks, leftLengthHor[0], image_w, image_h)
-        p2 = get_landmark_coords(landmarks, leftLengthHor[1], image_w, image_h)
+        p1 = get_landmark_coords(landmarks, recoVars.leftLengthHor[0], image_w, image_h)
+        p2 = get_landmark_coords(landmarks, recoVars.leftLengthHor[1], image_w, image_h)
         leftHor = find_distance(p1, p2)
         cv.line(frame, p1, p2, (255, 0, 0), 2)
 
-        p3 = get_landmark_coords(landmarks, leftLengthVert[0], image_w, image_h)
-        p4 = get_landmark_coords(landmarks, leftLengthVert[1], image_w, image_h)
+        p3 = get_landmark_coords(landmarks, recoVars.leftLengthVert[0], image_w, image_h)
+        p4 = get_landmark_coords(landmarks, recoVars.leftLengthVert[1], image_w, image_h)
         leftVert = find_distance(p3, p4)
         cv.line(frame, p3, p4, (255, 0, 0), 2)
 
         # Rechtes Auge
-        p5 = get_landmark_coords(landmarks, rightLengthHor[0], image_w, image_h)
-        p6 = get_landmark_coords(landmarks, rightLengthHor[1], image_w, image_h)
+        p5 = get_landmark_coords(landmarks, recoVars.rightLengthHor[0], image_w, image_h)
+        p6 = get_landmark_coords(landmarks, recoVars.rightLengthHor[1], image_w, image_h)
         rightHor = find_distance(p5, p6)
         cv.line(frame, p5, p6, (0, 255, 0), 2)
 
-        p7 = get_landmark_coords(landmarks, rightLengthVert[0], image_w, image_h)
-        p8 = get_landmark_coords(landmarks, rightLengthVert[1], image_w, image_h)
+        p7 = get_landmark_coords(landmarks, recoVars.rightLengthVert[0], image_w, image_h)
+        p8 = get_landmark_coords(landmarks, recoVars.rightLengthVert[1], image_w, image_h)
         rightVert = find_distance(p7, p8)
         cv.line(frame, p7, p8, (0, 255, 0), 2)
 
@@ -81,10 +68,10 @@ while True:
         blinkRatio = (leftBlinkRatio + rightBlinkRatio) / 2
 
           # FPS anzeigen
-        fps = 1 / (new_frame_time - prev_frame_time)
-        prev_frame_time = new_frame_time
+        fps = 1 / (recoVars.new_frame_time - recoVars.prev_frame_time)
+        recoVars.prev_frame_time = recoVars.new_frame_time
 
-        plt.update(frame, leftBlinkRatio = leftBlinkRatio, rightBlinkRatio = rightBlinkRatio, blinkRatio = blinkRatio, fps = fps)
+        plt.update(frame, leftBlinkRatio = leftBlinkRatio, rightBlinkRatio = rightBlinkRatio, blinkRatio = blinkRatio, fps = int(fps))
         
     cv.imshow("FaceDetection", frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
---
+++ biosignals/facialRecognition/recognitionVariables.py
@@ -0,0 +1,14 @@
+# Gesichtslandmark-IDs
+rightEyeLandmarks = [33, 246, 161, 160, 159, 158, 157, 173, 133, 155, 154, 153, 145, 144, 163, 7, 23]
+rightLengthHor = [27, 23]
+rightLengthVert = [130, 243]
+rightIris = [473, 474, 475, 476, 477]
+
+leftEyeLandmarks = [466, 398, 384, 385, 386, 387, 388, 466, 263, 249, 390, 373, 374, 380, 381, 382, 362]
+leftLengthHor = [257, 253]
+leftLengthVert = [359, 463]
+leftIris = [468, 469, 470, 471, 472]
+
+# Variables for FPS calculation 
+prev_frame_time = 0
+new_frame_time = 0

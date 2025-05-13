a13f51c5630813fd2937cce50a6681e04549c7dd
Fabian <103424111+MatrixRave@users.noreply.github.com>
2025-05-13 22:56:02 +0200

    feat(facialReco): add new landmark variables

--- biosignals/facialRecognition/recognitionVariables.py
+++ biosignals/facialRecognition/recognitionVariables.py
@@ -3,11 +3,16 @@ rightEyeLandmarks = [33, 246, 161, 160, 159, 158, 157, 173, 133, 155, 154, 153, 
 rightLengthHor = [159, 145]
 rightLengthVert = [130, 243]
 rightIris = [473, 474, 475, 476, 477]
+rightEyeOuter = 263
+
 
 leftEyeLandmarks = [466, 398, 384, 385, 386, 387, 388, 466, 263, 249, 390, 373, 374, 380, 381, 382, 362]
 leftLengthHor = [386, 374]
 leftLengthVert = [359, 463]
 leftIris = [468, 469, 470, 471, 472]
+leftEyeOuter = 33
+
+
 
 # Variables for FPS calculation 
 prev_frame_time = 0

af6ed9c11718c910ba0a49e09fa876cf902bf737
n-vent <github@ventulett.de>
2025-09-06 21:39:14 +0200

    Fix import and time issues when writing telemetry to database

--- telemetry /import_csv.py
+++ telemetry /import_csv.py
@@ -6,6 +6,8 @@ from influxdb_client import Point, WritePrecision
 from database import Database
 from tqdm import tqdm
 import time
+pd.set_option('display.max_columns', None)
+
 
 def extract_start_time_from_filename(filename: str) -> datetime:
     match = re.match(r"(\d{8})-(\d{6})", filename)
@@ -19,47 +21,52 @@ def import_csv_to_influx(file_path: str):
     filename = os.path.basename(file_path)
     start_time = extract_start_time_from_filename(filename)
 
-    # Read CSV, skipping first 20 lines
-    df = pd.read_csv(file_path, skiprows=18)
-    #remove units
+    # Read CSV, skipping first 18 lines to have a leading row with headers
+    df = pd.read_csv(file_path, skiprows=18, low_memory=False)
+
+    #remove row containing units
     df = df.iloc[1:].reset_index(drop=True)
+
+    # set dtypes
+    df = df.apply(lambda s: pd.to_numeric(s) if s.dtype == object else s)
 
     print(df.head())
     # First column is duration in milliseconds
     duration_col = df.columns[0]
-    df["timestamp"] = [start_time + timedelta(milliseconds=float(val)) for val in df[duration_col]]
 
-    buffer = []
-    for _, row in tqdm(df.iterrows()):
-        point = (
-            Point("csv_import")  # measurement name
-            .time(row["timestamp"], WritePrecision.NS)
-        )
+    df["timestamp"] = [start_time + timedelta(seconds=float(val)) for val in df[duration_col]]
+
+    measurement_name = "csv_import"
+    for _, row in tqdm(df.iterrows(), total=len(df)):
+        tags = {
+            "device": "simulator"
+        }
+        ts = pd.to_datetime(row['timestamp'])
+        if ts.tzinfo is None:
+            ts = ts.tz_localize("Europe/Berlin")
+        timestamp_utc = ts.tz_convert("UTC")
+
+        fields = {}
         for col in df.columns:
             if col not in [duration_col, "timestamp"]:
                 value = row[col]
                 if pd.notna(value):
                     try:
-                        point.field(col, float(value))
+                        fields.update({str(col): float(value)})
                     except ValueError:
-                        point.field(col, str(value))
-        buffer.append(point)
+                        fields.update({str(col): str(value)})
 
-        # Write batch when buffer is full
-        if len(buffer) >= database.BATCH_SIZE:
-            database.write_batch(buffer)
-            buffer = []
-
-    # Write any remaining points
-    if buffer:
-        database.write_batch(buffer)
+        database.write_record(measurement_name,
+                              tags,
+                              fields,
+                              timestamp_utc)
 
     time.sleep(1)
 
 
 def import_multiple(files):
     for f in files:
-        print("Importing telemtry from", f)
+        print("Importing telemetry from:", f)
         import_csv_to_influx(f)
 
 if __name__ == "__main__":
